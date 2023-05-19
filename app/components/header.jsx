2d20ae8cbf9567ba4c78d7366b264b37e0525b0f
Eliana Ayelén Granado <eligranado43@gmail.com>
2023-05-19 11:15:37 -0500

    Creacion header & footer

---
+++ app/components/header.jsx
@@ -0,0 +1,19 @@
+import { Link } from "@remix-run/react"
+import logo from "public/img/logo.svg"
+import Navegation from "./navegation";
+
+function Header() {
+
+  return (
+    <header className="header">
+      <div className="contenedor barra">
+        <Link to="/">
+            <img className="logo" src={logo} alt="Imagen logo" />
+        </Link>
+        <Navegation/>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ app/routes/tienda.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Tienda() {
+  return (
+    <>Tienda</>
+  )
+}
+
+export default Tienda
